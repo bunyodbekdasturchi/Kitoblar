344dd4e18b53018d86f6472fcc01bd56a6356ce8
bunyodbekdasturchi <qadamboyev061@gmail.com>
2026-03-14 15:09:10 +0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class books(models.Model):
+    nomi=models.CharField(max_length=40)
+    mualif=models.CharField(max_length=50)
+    narx=models.DecimalField(max_digits=60, decimal_places=3)
+
+    def __str__(self):
+            return self.nomi
+
+
+class xaridor(models.Model):
+    ism=models.CharField(max_length=30)
+    famila=models.CharField(max_length=30)
+    books=models.ForeignKey(books,on_delete=models.CASCADE)
+
+    def __str__(self):
+            return self.nomi
+
+
+
+
+
+
+
+
+
+
