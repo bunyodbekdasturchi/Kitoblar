ece456c383e5c03fb7ed03e08c5f0cd8349850ab
bunyodbekdasturchi <qadamboyev061@gmail.com>
2026-03-14 15:45:01 +0500

    Add files via upload

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class books(models.Model):
+class telefon(models.Model):
     nomi=models.CharField(max_length=40)
-    mualif=models.CharField(max_length=50)
+    model=models.CharField(max_length=50)
     narx=models.DecimalField(max_digits=60, decimal_places=3)
 
     def __str__(self):
@@ -12,17 +12,7 @@ class books(models.Model):
 class xaridor(models.Model):
     ism=models.CharField(max_length=30)
     famila=models.CharField(max_length=30)
-    books=models.ForeignKey(books,on_delete=models.CASCADE)
+    books=models.ForeignKey(telefon,on_delete=models.CASCADE)
 
     def __str__(self):
             return self.nomi
-
-
-
-
-
-
-
-
-
-
